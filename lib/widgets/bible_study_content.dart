0522bba7c917f1a810d8c233fb00df41cae03e58
Joelodufu <joel.odufu@gmail.com>
2022-03-31 03:17:48 +0100

    Without responsible data

--- lib/widgets/bible_study_content.dart
+++ lib/widgets/bible_study_content.dart
@@ -4,31 +4,31 @@ import 'app_text.dart';
 
 class BibleStudyContent extends StatelessWidget {
   List bibleStudyList;
-   BibleStudyContent({ Key? key, required this.bibleStudyList }) : super(key: key);
+  BibleStudyContent({Key? key, required this.bibleStudyList}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-                          controller: ScrollController(
-                            // or whatever offset you wish
-                          ),
-                          itemBuilder: (context, index) => Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                                AppText(
-                                  text: "${bibleStudyList[0].topic}",
-                                  color: Colors.red,
-                                ),
-                                AppText(
-                                  text: "Hello to the whole world",
-                                  size: 25,
-                                ),
-                              ]),
-                          itemCount: bibleStudyList.length,
-                        );
+      controller: ScrollController(
+          // or whatever offset you wish
+          ),
+      itemBuilder: (context, index) => Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            AppText(
+              text: "${bibleStudyList[0].topic}",
+              color: Colors.red,
+            ),
+            AppText(
+              text: "Hello to the whole world",
+              size: 25,
+            ),
+          ]),
+      itemCount: bibleStudyList.length,
+    );
   }
 }
